c5e75c356b6b330718b1c98c9d5cd2490c13150a
Pritesh Vardekar <priteshv.264@gmail.com>
2023-12-22 00:49:32 +0530

    first version done.

---
+++ script.js
@@ -0,0 +1,57 @@
+
+var timer = 60;
+var hitnum =0;
+var score =0;
+var setTimeInterval;
+
+function makeBubbles(){
+    let clutter = "";
+for(let i=1;i<=147;i++){
+    let rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`
+}
+
+document.querySelector("#container-btm").innerHTML= clutter;
+}
+
+function runTimer(){
+    if(!setTimeInterval){
+        setTimeInterval = setInterval(() => {
+            if(timer>0){
+              timer--;
+              document.querySelector("#timer").innerHTML=timer;
+            }
+            else{
+           clearInterval(setTimeInterval);
+           document.querySelector("#container-btm").innerHTML=`  <h1 id="gameoverh1">Game Over!</h1>`;
+           document.querySelector("#sethit").innerHTML=0;
+            }
+          }, 1000);
+    }
+}
+
+function runHit(){
+     hitnum = Math.floor((Math.random()*10));
+    document.querySelector("#sethit").innerHTML= hitnum;
+    
+}
+
+function increaseScore(){
+  score += 10;
+  document.querySelector("#score").innerHTML=score;
+}
+
+document.querySelector("#container-btm").addEventListener("click",function(details){
+    document.querySelector("#startgameh3").innerHTML="";
+    runTimer();
+    var clickednum = Number(details.target.innerHTML);
+      if(clickednum==hitnum){
+        increaseScore();
+        makeBubbles();
+        runHit();
+      }
+     
+})
+
+ 
+
